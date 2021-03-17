fb760e814062c9dbea77191f03c2a1090c87de31
Zack H <zphixon@gmail.com>
2021-03-16 22:14:59 -0400

    update ast

--- src/p.rs
+++ src/p.rs
@@ -4,8 +4,76 @@ use super::*;
 
 /// syntax tree
 pub(crate) enum St<'a> {
+  /// assign
+  As {
+    /// name
+    n: T<'a>,
+    /// expression
+    e: Ex<'a>,
+  },
+  /// assign quote
+  Aq {
+    /// name
+    n: T<'a>,
+    /// expression
+    e: Ex<'a>,
+  },
+  Ex(Ex<'a>),
+}
+
+pub(crate) enum Ex<'a> {
+  /// identifier
+  I {
+    /// name
+    n: T<'a>,
+  },
+  /// list
+  L {
+    /// value
+    v: Vec<T<'a>>,
+  },
+  /// monad call
+  M {
+    /// operator
+    op: Box<Ex<'a>>,
+    /// operand
+    lhs: Box<Ex<'a>>,
+  },
+  /// dyad call
+  D {
+    /// rhs operand
+    rhs: Box<Ex<'a>>,
+    /// operator
+    op: Box<Ex<'a>>,
+    /// lhs operand
+    lhs: Box<Ex<'a>>,
+  },
+  /// quote
+  Q {
+    /// colon
+    co: T<'a>,
+    /// expression
+    e: Box<Ex<'a>>,
+  },
+  /// call
+  C {
+    e: Box<Ex<'a>>,
+    co: T<'a>,
+  },
+  /// spread
+  Sp {
+    /// verb
+    v: Box<Ex<'a>>,
+    /// slash
+    s: T<'a>,
+  },
   /// anon function
-  Dvn(Vec<T<'a>>),
+  Dfn {
+    /// left brace
+    lb: T<'a>,
+    /// expr
+    e: Box<Ex<'a>>
+  },
 }
 
 pub(crate) fn p() -> O<St<'static>> {
@@ -15,11 +83,31 @@ pub(crate) fn p() -> O<St<'static>> {
 #[cfg(test)]
 mod t {
   use super::*;
+
   #[test]
   fn p() {
     let mut s = Sc::n("x+y");
     let v: Vec<_> = s.collect();
     assert_eq!(v, vec![T::n(Tk::I(0), "x"), T::n(Tk::I(0), "+"), T::n(Tk::I(0), "y")]);
+  }
+
+  #[test]
+  fn p2() {
+    let src = "a = 1 + 2";
+    let a = St::As {
+      n: T::n(Tk::I(0), "a"),
+      e: Ex::D {
+        rhs: Box::new(Ex::L {
+          v: vec![T::n(Tk::Dg, "1")],
+        }),
+        op: Box::new(Ex::I {
+          n: T::n(Tk::I(0), "+")
+        }),
+        lhs: Box::new(Ex::L {
+          v: vec![T::n(Tk::Dg, "2")]
+        }),
+      },
+    };
   }
 }
 
